3168306b7c6c91351a55acb8f668f4c951ee3ba1
paul.jeoung <kuni5458@naver.com>
2025-02-21 13:43:45 +0900

    featuer - 토글유아이 위로 올라가는거, 환경에 따라서 동작 하게 변경

--- src/components/home/Toggler.js
+++ src/components/home/Toggler.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Box} from "@mui/material";
+import {Box, useTheme} from "@mui/material";
 
 export default function Toggler({darkMode, handleClick}) {
     const transition = 'all 250ms ease'
+    const theme = useTheme()
 
    return (
-      <Box fontSize={'1.5rem'} sx={{cursor: 'pointer', ":hover": {transform: 'translateY(-3px)', transition: transition}}}>
+      <Box fontSize={'1.5rem'} sx={{cursor: 'pointer', transition: transition, 
+         [theme.breakpoints.up('md')] : { ":hover": {transform: 'translateY(-3px)' }},
+         [theme.breakpoints.down('md')] : { ":hover": {transform: 'translateY(3px)'}}, 
+         }} > 
          {
             darkMode ?
                <span onClick={handleClick} aria-label="Full Moon" role="img">🌕</span>
